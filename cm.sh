07757608a11c4b5134e0643b13199b7a6a65998d
LorneWhiteway <lorne.whiteway.13@ucl.ac.uk>
2019-11-06 08:43:00 +0000

    CCfits module file now sets environment variable.

--- cm.sh
+++ cm.sh
@@ -4,8 +4,7 @@ rm -rf ./build
 mkdir build
 cd build
 
-# Note that the splinter module file for CCfits doesn't (yet) define an environment variable for the package location.
-cmake .. -DCFITSIO_ROOT_DIR=${FITSIO_DIR} -DFFTW_ROOT=${FFTW_ROOT} -DNFFT_INCLUDE_DIR=${NFFT_HOME}/include -DNFFT_LIBRARY=${NFFT_HOME}/lib/libnfft3.a -DCCFITS_ROOT_DIR=/share/splinter/cosmos/modules/may_2017/install_dir/CCfits-2.5/ -DARMADILLO_INCLUDE_DIR=${ARMADILLO_HOME}/include -DARMADILLO_LIBRARY=${ARMADILLO_HOME}/lib64/libarmadillo.so
+cmake .. -DCFITSIO_ROOT_DIR=${FITSIO_DIR} -DFFTW_ROOT=${FFTW_ROOT} -DNFFT_INCLUDE_DIR=${NFFT_HOME}/include -DNFFT_LIBRARY=${NFFT_HOME}/lib/libnfft3.a -DCCFITS_ROOT_DIR=${CCFITS_ROOT_DIR} -DARMADILLO_INCLUDE_DIR=${ARMADILLO_HOME}/include -DARMADILLO_LIBRARY=${ARMADILLO_HOME}/lib64/libarmadillo.so
 
 #-DUSE_FFTW=ON -DCOMPILE_NFFT=ON
 
