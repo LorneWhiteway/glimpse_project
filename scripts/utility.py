d35dafaed4733b434f71e13b1affce2e04d7aac8
LorneWhiteway <lorne.whiteway.13@ucl.ac.uk>
2019-11-26 10:33:53 +0000

    Ongoing development of ./scripts/utility.py.

--- scripts/utility.py
+++ scripts/utility.py
@@ -86,7 +86,7 @@ def save_buzzard_truth():
     
     do_nest = False
     
-    nside = 1024
+    nside = 512
     num_healpixels = hp.nside2npix(nside)
     sums = np.zeros(num_healpixels)
     count = np.zeros(num_healpixels)
@@ -110,7 +110,7 @@ def save_buzzard_truth():
     average_values = np.divide(sums, count, out=np.zeros_like(count, dtype=float), where=count!=0.0)
     
     # Save
-    filename = "/share/splinter/ucapwhi/glimpse_project/output/Buzzard_192.nside1024_truth.dat"
+    filename = "/share/splinter/ucapwhi/glimpse_project/output/Buzzard_192.nside" + str(nside) + "_truth.dat"
     
     hp.write_map(filename, average_values, do_nest, overwrite=True)
     
@@ -119,9 +119,16 @@ def compare_two_healpix_maps():
 
     import healpy as hp
     import matplotlib.pyplot as plt
+    
+    nside = 512
 
     path = "/share/splinter/ucapwhi/glimpse_project/output/"
-    filenames = ["Buzzard_192.nside1024_merged_values.dat", "Buzzard_192.nside1024_truth.dat"]
+    filenames = ["Buzzard_192.nside" + str(nside) + "_merged_B_values.dat", "Buzzard_192.nside" + str(nside) + "_truth.dat"]
+    
+    show_weights = True
+    if show_weights:
+        filenames.append("Buzzard_192.nside" + str(nside) + "_merged_B_weights.dat")
+    
     maps = []
     titles = []
     for f in filenames:
@@ -131,16 +138,22 @@ def compare_two_healpix_maps():
     smooth_truth = True
     if smooth_truth:
         one_arcmin_in_radians = 0.000290888
-        smoothing_scale_in_arcmin = 15.0
+        smoothing_scale_in_arcmin = 7.0
         maps[1] = hp.smoothing(maps[1], sigma = smoothing_scale_in_arcmin * one_arcmin_in_radians)
         titles[1] += " smoothed at {} arcmin".format(smoothing_scale_in_arcmin)
     
-    maps.append(maps[0]-maps[1])
-    titles.append("Diff")
+    show_diff = False
+    if show_diff:
+        maps.append(maps[0]-maps[1])
+        titles.append("Diff")
+    
+    show_weights = True
+        
+    
     
     for map, title, i in zip(maps, titles, range(len(maps))):
         #hp.mollview(map, fig=i, title=title)
-        hp.gnomview(map, fig=i, title=title)
+        hp.gnomview(map, fig=i, title=title, reso=2.0)
         hp.graticule()
     
     plt.show()
@@ -153,17 +166,19 @@ def clean_up_edges():
     import matplotlib.pyplot as plt
     
     do_nest = False
+    
+    nside = 512
 
     path = "/share/splinter/ucapwhi/glimpse_project/output/"
-    old_filename = "Buzzard_192.nside1024_merged_values.dat"
-    new_filename = "Buzzard_192.nside1024_merged_values_masked.dat"
-    new_filename_png = "Buzzard_192.nside1024_merged_values_masked.png"
+    old_filename = "Buzzard_192.nside" + str(nside) + "_merged_B_values.dat"
+    new_filename = "Buzzard_192.nside" + str(nside) + "_merged_B_values_masked.dat"
+    new_filename_png = "Buzzard_192.nside" + str(nside) + "_merged_B_values_masked.png"
     
     m = hp.read_map(path + old_filename)
     (ra, dec) = get_fits_data(buzzard_data_file_name(), ["RA", "DEC"])
     
     # Create a mask that is zero in healpixels where there are no source galaxies and one elsewhere.
-    nside = 1024
+    
     ids = hp.ang2pix(nside, ra, dec, do_nest, True)
     mask = np.zeros(hp.nside2npix(nside))
     for id in ids:
@@ -178,17 +193,77 @@ def clean_up_edges():
     hp.mollview(m, title=new_filename)
     plt.savefig(path + new_filename_png)
     
+
+
+
+def sphere_to_tangent_plane_mapping(ra_centre, dec_centre, ra, dec):
+    import numpy as np
+    
+    # See my notes p. GL90
+    
+    ra_centre_rad = np.radians(ra_centre)
+    dec_centre_rad = np.radians(dec_centre)
+    ra_rad = np.radians(ra)
+    dec_rad = np.radians(dec)
+    
+    x = np.sin(ra_rad - ra_centre_rad) / (np.cos(dec_centre_rad) * np.cos(ra_rad - ra_centre_rad) + np.tan(dec_rad) * np.sin(dec_centre_rad))
+    
+    epsilon = np.sin(dec_rad) * np.cos(dec_centre_rad) - np.cos(dec_rad) * np.sin(dec_centre_rad) * np.cos(ra_rad - ra_centre_rad)
+    
+    y = epsilon * np.sqrt((1.0 + x**2) / (1.0 - epsilon**2))
+    
+    return (x, y)
+    
+
     
     
+def sphere_to_tangent_plane_mapping_test_harness():
+
+    import numpy as np
+    
+    pixel_id = 2218
+    ra_centre = 56.25
+    dec_centre = -27.27961273597809
+
+    glimpse_output_file = "/share/splinter/ucapwhi/glimpse_project/output/Buzzard_192." + str(pixel_id) + ".glimpse.out.fits"
+    (ra, dec, kappa) = get_from_glimpse(glimpse_output_file)
+    array_size = int(np.sqrt(len(ra)))
     
     
+    if True:
+    
+        (x, y) = sphere_to_tangent_plane_mapping(ra_centre, dec_centre, ra, dec)
+    
+        x_new = np.reshape(x, [array_size, array_size])
+        y_new = np.reshape(y, [array_size, array_size])
+
+        step_size = x_new[0, 1] - x_new[0, 0]
+        
+        max_error = 0.0
+        for i in range(array_size):
+            for j in range(array_size):
+                error_x = x_new[i][j] - step_size*(j - array_size/2 + 0.5)
+                error_y = y_new[i][j] - step_size*(i - array_size/2 + 0.5)
+                error = np.sqrt(error_x**2 + error_y**2)
+                max_error = max(max_error, error)
+                
+                
+        print("Max error = {}".format(max_error))
+        
+    if True:
+    
+        ra_new = np.reshape(ra, [array_size, array_size])
+        dec_new = np.reshape(dec, [array_size, array_size])
+        
+        for i in [0, array_size-1]:
+            for j in [0, array_size-1]:
+                print("RA = {}; DEC = {}".format(ra_new[i][j], dec_new[i][j]))
+        
     
     
+
     
-    
-    
-    
-    
+            
     
     
     
@@ -201,4 +276,5 @@ if __name__ == '__main__':
     #tester()
     #save_buzzard_truth()
     #compare_two_healpix_maps()
-    clean_up_edges()
+    #clean_up_edges()
+    sphere_to_tangent_plane_mapping_test_harness()
