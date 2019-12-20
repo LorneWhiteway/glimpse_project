98f5f462bc95f54a37d28fa58713cbb87cbf9b29
LorneWhiteway <lorne.whiteway.13@ucl.ac.uk>
2019-12-20 16:07:47 +0000

    Fixed typo.

--- scripts/utility.py
+++ scripts/utility.py
@@ -1111,9 +1111,39 @@ def create_cutouts_caller(ini_file_name, job_control):
 
 ######################### End of create_cutouts code #########################
 
+######################### Start of glimpse_caller code #########################
 
+def glimpse_caller(ini_file_name, job_id):
 
+    import glob
+    import subprocess
+    import configparser
+    
+    config = configparser.ConfigParser()
+    config.read(ini_file_name)
+    
+    output_directory = config["project"].get("directory")
+    project = config["project"].get("project_name")
+    exe_file = config["project"].get("glimpse_executable")
+    
+    glimpse_cat_file_pattern = output_directory + project + ".*.glimpse.cat.fits"
+    id_list = [int(f.split(".")[-4]) for f in glob.glob(glimpse_cat_file_pattern)]
+    id_list.sort()
+    this_healpix_pixel_id = id_list[int(job_id)]
+    
+    print("Healpixel id = {}...".format(this_healpix_pixel_id))
+    
+    this_healpix_id_as_string = str(this_healpix_pixel_id).zfill(4)
+    
+    ini_file = output_directory + project  + ".glimpse.ini"
+    cat_file = output_directory + project + "." + this_healpix_id_as_string + ".glimpse.cat.fits"
+    out_file = output_directory + project + "." + this_healpix_id_as_string + ".glimpse.out.fits"
 
+    subprocess.run([exe_file, ini_file, cat_file, out_file])
+
+    print("Finished.")
+    
+######################### End of glimpse_caller code #########################
 
 
 ######################### Start of merge code #########################
