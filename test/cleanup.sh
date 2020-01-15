3deedd19eeea1e747d335f5fe3abcabfec77026e
LorneWhiteway <lorne.whiteway.13@ucl.ac.uk>
2020-01-15 17:01:09 +0000

    Changes to file names for ./test.

--- test/cleanup.sh
+++ test/cleanup.sh
@@ -4,6 +4,4 @@ cd /share/splinter/ucapwhi/glimpse_project/test/
 rm *.out
 rm *.cat.fits
 rm *.out.fits
-rm *.dat
-
  
--- test/run_test.sh
+++ test/run_test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
- ../scripts/glimpse_on_curved_sky.py -i ./glimpse.ini -t create_cutouts
+ ../scripts/glimpse_on_curved_sky.py -i ./control.ini -t create_cutouts
  sbatch --wait --array=0-16 ./job_script_glimpse_caller
  wait
- ../scripts/glimpse_on_curved_sky.py -i ./glimpse.ini -t merge
+ ../scripts/glimpse_on_curved_sky.py -i ./control.ini -t merge
  
