011367dfa839ca38c55142aefb411cc52fb6d2ad
LorneWhiteway <lorne.whiteway.13@ucl.ac.uk>
2020-01-15 17:12:07 +0000

    regression test batch file now does diff.

--- regression_test/run_regression_test.sh
+++ regression_test/run_regression_test.sh
@@ -4,4 +4,5 @@
  sbatch --wait --array=0-16 ./job_script_glimpse_caller
  wait
  ../scripts/glimpse_on_curved_sky.py -i ./control.ini -t merge
- 
+diff ./expected.glimpse.merged.values.dat ./glimpse.merged.values.dat 
+diff ./expected.glimpse.merged.weights.dat ./glimpse.merged.weights.dat 
